1e7453017e45e6701b3b03f388c19e5c302d422f
Harrison Nicholls <harrison.nicholls@physics.ox.ac.uk>
2025-04-25 20:46:00 +0200

    Fix tests

--- tests/test_init.py
+++ tests/test_init.py
@@ -14,23 +14,23 @@ def test_list_cmaps():
     assert isinstance(cmaps, list)
     assert len(cmaps) > 0
     assert all(isinstance(cmap, str) for cmap in cmaps)
-    assert all(cmap in cmaps for cmap in ["spotted_pardalote", "plains_wanderer", "bee_eater"])
+    assert all(cmap in cmaps for cmap in ["Cezanne", "vanGogh", "Turner"])
 
 def test_get_cmap():
     # Get from package
-    cmap = lisa.get_cmap("spotted_pardalote")
+    cmap = lisa.get_cmap("Rembrandt")
     assert isinstance(cmap, LinearSegmentedColormap)
-    assert cmap.name == "spotted_pardalote"
+    assert cmap.name == "Rembrandt"
     assert cmap(0.0)
 
     # Test reverse colormap
-    cmap_r = lisa.get_cmap("spotted_pardalote_r")
+    cmap_r = lisa.get_cmap("Rembrandt_r")
     assert isinstance(cmap_r, LinearSegmentedColormap)
-    assert cmap_r.name == "spotted_pardalote_r"
+    assert cmap_r.name == "Rembrandt_r"
     assert cmap_r(1.0) == cmap(0.0)
 
 def test_registered_cmap():
     # Check if the colormap is registered with matplotlib
-    cmap = plt.get_cmap("spotted_pardalote")
+    cmap = plt.get_cmap("Warhol")
     assert isinstance(cmap, LinearSegmentedColormap)
-    assert cmap.name == "spotted_pardalote"
+    assert cmap.name == "Warhol"
